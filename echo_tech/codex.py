14e61f97404c197d01e383b0dbc54a2b1159c19d
Hadi Aji <hadeaji@gmail.com>
2020-12-25 17:20:18 +0200

    initial commit

--- echo_tech/codex.py
+++ echo_tech/codex.py
@@ -2,15 +2,12 @@ import cv2
 import numpy as np
 import types
 
-# def get_image(path):
-#     with open(path,'rb') as imf:
-#         image = imf.read()
-#     return image
-
 
 def messege_to_binary(message):
     if type(message) == str:
         return ''.join([format(ord(i), "08b") for i in message])
+    elif type(message) == bytes or type(message) == np.ndarray:
+        return [format( i, "08b") for i in message]
     elif type(message) == int or type(message) == np.uint8:
         return format(message, "08b")
     else:
@@ -51,6 +48,67 @@ def encode(image, new_message):
                 break
     return image
 
-# print(cv2.imread('./assets/test.png').shape[1])
 
 
+
+def show_data(image):
+    binary_data= ""
+
+    for values in image:
+        for pixel in values:
+            # converting the r,g,b into binary format 
+            r, g, b = messege_to_binary(pixel)
+            binary_data += r[-1]
+            binary_data += g[-1]
+            binary_data += b[-1]
+
+    all_bytes=[binary_data[i:i+8] for i in range(0,len(binary_data), 8)]
+
+    # converting the bits into characters 
+
+    decoded_data = ""
+    for byte in all_bytes:
+        decoded_data += chr(int(byte ,2))
+        if decoded_data[-5:] == "#####":
+            break
+    return decoded_data[:-5] 
+
+
+def encode_text():
+
+    image_name = input("Insert an image(with the extention):  ") 
+    image = cv2.imread(image_name)  #transforming image into matrix of r,g,b 
+
+    print('the shape of the image is :', image.shape)
+    
+    data = input('Enter your secret message ^.^')
+    if (len(data) == 0 ):
+        raise ValueError ('there is no message ,, sorry  !!')
+
+    file_name = input('Enter the name of the new image(with the extention): ')
+    encode_image = encode(image, data)
+    cv2.imwrite(file_name, encode_image)
+
+
+def decode_text():
+
+    image_name = input("Insert an image(with the extention) you want to decode:  ") 
+    image = cv2.imread(image_name)  #transforming image into matrix of r,g,b 
+ 
+    text = show_data(image)
+    return text
+
+
+def Main():
+    a = input("Steganography, Please press the number of the task needed \n 1.Encode \n 2.Decode \n Option Number: ")
+    userinput = int(a)
+    if (userinput == 1):
+        print("\n Encoding..")
+        encode_text()
+    elif (userinput == 2):
+        print("\n Decoding..")
+        print("Decoded message is" + decode_text())
+    else:
+        raise Exception("Please try again")
+
+Main()
