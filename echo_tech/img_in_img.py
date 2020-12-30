a45c23db6e3afa67aa56a251ce1cd656c9814466
Dianashafee <diana.shafee1997@hotmail.com>
2020-12-30 02:07:17 +0200

    finishing the img inside image code ^.^

--- echo_tech/img_in_img.py
+++ echo_tech/img_in_img.py
@@ -32,10 +32,39 @@ def encrypt():
                 img1[i][j][l]= int(v3, 2) 
     cv2.imwrite('pic3in2.png', img1) 
   
+# Decryption function 
+def decrypt(): 
       
+    # Encrypted image 
+    img = cv2.imread('pic3in2.png')  
+    width = img.shape[0] 
+    height = img.shape[1] 
+      
+    # img1 and img2 are two blank images 
+    img1 = np.zeros((width, height, 3), np.uint8) 
+    img2 = np.zeros((width, height, 3), np.uint8) 
+      
+    for i in range(width): 
+        for j in range(height): 
+            for l in range(3): 
+                v1 = format(img[i][j][l], '08b') 
+                v2 = v1[:4] + chr(random.randint(0, 1)+48) * 4
+                v3 = v1[4:] + chr(random.randint(0, 1)+48) * 4
+                  
+                # Appending data to img1 and img2 
+                img1[i][j][l]= int(v2, 2) 
+                img2[i][j][l]= int(v3, 2) 
+      
+    # These are two images produced from 
+    # the encrypted image 
+    cv2.imwrite('pic2_re.png', img1) 
+    cv2.imwrite('pic3_re.png', img2)
+
+
 
 # Driver's code 
 encrypt() 
+decrypt()      
 
 
 
